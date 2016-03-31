f9ceab0c7b5cd7c47587c5f9329f476499676a54
thypad <thyago.porpino@gmail.com>
2016-03-31 09:54:24 -0300

    improving description of package

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='bovespa',
       version='0.1.0',
-      description='reads bovespa historic quote files',
+      description='Reads historical quote files from BM&FBovespa (Brazillian Stock Exchange)',
       url='http://github.com/thypad/bovespa',
       author='Thyago Neves Porpino',
       author_email='thyago.porpino@gmail.com',
       license='MIT',
       packages=find_packages(),
       zip_safe=False)
-
