ebc9903b6e4691b1c63326e08e73c0fe1bd7574b
Thyago Porpino <thyago.porpino@gmail.com>
2016-06-01 23:47:10 -0300

    sketching up class for a stock exchange

--- bovespa/__init__.py
+++ bovespa/__init__.py
@@ -9,3 +9,11 @@ def stock_history(filepaths=None, stock_code=None):
         recs.add(bf.stockquotes(code=stock_code))
 
     return recs
+
+
+class Bovespa:
+    def __init__(self):
+        pass
+
+    def companies(self):
+        pass
