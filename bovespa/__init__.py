e093c9c1f15c20eaaafe19ee4f094061685a58aa
Thyago Porpino <thyago.porpino@gmail.com>
2016-06-03 23:48:03 -0300

    trying some new stuff in regards to data storage

--- bovespa/__init__.py
+++ bovespa/__init__.py
@@ -1,6 +1,8 @@
 from .utils import record
 from .io import file
 
+import sqlite3
+
 def stock_history(filepaths=None, stock_code=None):
     recs = record.RecordCollection()
 
@@ -13,7 +15,21 @@ def stock_history(filepaths=None, stock_code=None):
 
 class Bovespa:
     def __init__(self):
+        self.cursor = None
         pass
 
+    def connect(sqlite_file):
+        conn = sqlite3.connect(sqlite_file)
+        self.cursor = conn.cursor()
+
+    def create_database(filelist, out=None):
+        # create database from files
+
+        conn = sqlite3.connect(out)
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE company (name, cnpj, sector)''')
+
+
     def companies(self):
+        cursor.execute('''CREATE TABLE company (name, cnpj, sector)''')
         pass
