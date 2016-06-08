656ff9ac3281abf2099b564ef0fdee981d185e59
Thyago Porpino <thyago.porpino@gmail.com>
2016-06-07 23:59:30 -0300

    add table stockquotes

--- bovespa/models.py
+++ bovespa/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 
 
 engine = create_engine('sqlite:///:memory:', echo=True)
@@ -14,6 +14,22 @@ class Company(Base):
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
 
     name = Column(String(50))
+    cnpj = Column(String(50))
+    sector = Column(String(12))
+
+    def __repr__(self):
+    	return "<User(name='%s', fullname='%s', password='%s')>" % (
+                             self.name, self.fullname, self.password)
+
+
+class Stockquote(Base):
+    __tablename__ = 'stockquotes'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+
+    stock_code = Column(String(50))
+
+    price_open = Column(Float
     cnpj = Column(String(50))
     sector = Column(String(12))
 
